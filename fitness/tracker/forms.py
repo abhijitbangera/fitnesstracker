406b2a0533ced0d59ee39d5c1a575e179aceb595
Abhijit <abhijit.bangera@hotmail.com>
2016-03-31 22:16:13 +0530

    weight tracker working

--- fitness/tracker/forms.py
+++ fitness/tracker/forms.py
@@ -5,3 +5,4 @@ class basictrackerForm(forms.ModelForm):
 	class Meta:
 		model=basictracker
 		fields = '__all__'
+		exclude = ["user"]
--- fitness/tracker/views.py
+++ fitness/tracker/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 from tracker.forms import basictrackerForm
 from django.core.context_processors import csrf
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+@login_required
 def basictracker(request):
-	if request.POST:
-		print("POST HUA!!!!")
+	if request.method=='POST':
 		form= basictrackerForm(request.POST)
 		if form.is_valid():
-			form.save()
+			save_it=form.save(commit = False)
+			save_it.user = request.user
+			save_it.save()
 			print("saved successfully.")
 			return HttpResponseRedirect("/")
 	else:
-		print("testing")
 		form=basictrackerForm()
 	context={}
 	context.update(csrf(request))
