87fa715a7d627be8ee057d580ca8ea65245d62ea
Abhijit <abhijit.bangera@hotmail.com>
2016-07-03 16:31:01 +0530

    disable instamojo app

--- fitness/fitness/settings.py
+++ fitness/fitness/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'tracker',   
   
-    'instamojo-py-master',
+    # 'instamojo-py-master',
     'postman',
 
 ]
--- fitness/fitness/urls.py
+++ fitness/fitness/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', 'tracker.views.dashboard'),
